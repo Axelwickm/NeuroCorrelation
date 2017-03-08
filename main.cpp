c91d8ac3d8de950a629fe8042841c4509b0bc45e
Axel Wickman <axelwickm@gmail.com>
2017-03-08 23:23:48 +0100

    Different scoped examples

--- main.cpp
+++ main.cpp
@@ -18,60 +18,109 @@ int main(int argc, char* args[]){
     srand(seed);
     std::cout<<"Current seed: "<<seed<<std::endl;
 
-    NeuCor brain(500);
+    enum currentSimulationEnum {SIM_OTHER, SIM_FEW_NEURONS, SIM_300_NEURONS};
+    const currentSimulationEnum currentSimulation = SIM_300_NEURONS;
 
-/*
-    for (float xpos = 0; xpos<8; xpos++){
-        coord3 d;
-        if (xpos == 0) d = {0.5, 0, 0};
-        if (xpos == 1) d = {0, 0.5, 0};
-        if (xpos == 2) d = {0, 0, 0.5};
-        if (xpos == 3) d = {0, 0, 0.6};
-        if (xpos == 4) d = {0, 0, 0.8};
-        if (xpos == 5) d = {0, 0, 0.7};
-        if (xpos == 5) d = {0.6, 0, 0};
-        if (xpos == 6) d = {0.4, 0.1, 0};
-        if (xpos == 7) d = {0.4, 0.1, 0};
-        brain.createNeuron(d);
-    }
-    brain.makeConnections();
-*/
-
-    NeuCor_Renderer brainRenderer(&brain);
-    brainRenderer.runBrainOnUpdate = true;
-    brainRenderer.realRunspeed = false;
-    brainRenderer.setDestructCallback(windowDestroy);
-
-    brain.runAll = true;
-    brain.runSpeed = 0.05;
+    switch (currentSimulation){
+        case SIM_FEW_NEURONS:{
+            NeuCor brain(0);
 
 
-    float inputs[] = {0, 0, 200};
-    coord3 inputPositions[] = {{1,0,0},{-2,0,0}, {-0.5, 1.5, 0}};
-    float inputRadius[] = {0.75, 1.0, 0.75};
-    brain.setInputRateArray(inputs, sizeof(inputs)/sizeof(float), inputPositions, inputRadius);
+            brain.runAll = true;
+            brain.runSpeed = 0.01;
 
-    std::cout<<"Starting program loop\n";
-    unsigned t = 0;
-    do {
-        t++;
-        if (false) brain.run();
+            coord3 neuronPositions[] = {{0,0,0}, {-0.52,-0.5,0}};//, {0.52,-0.5,0}, {0,0.8,0}};
+            for (auto &n: neuronPositions) brain.createNeuron(n);
 
-        //if (brain.getTime() > 20){inputs[0] = 0; inputs[1] = 0;}
-        /*if (sin(brain.getTime()/30.0) > 0.0){
-            inputs[0] = 100; inputs[1] = 100;
+            /*brain.createSynapse(3, 0, 1.f);*/ brain.createSynapse(0, 1, 0.9f);// brain.createSynapse(0, 2, 0.2f);
+
+            coord3 inputPositions[] = {neuronPositions[1]};//, neuronPositions[2]};
+            float inputRadius[] = {0.1};
+            float inputs[] = {200};
+            brain.setInputRateArray(inputs, sizeof(inputs)/sizeof(float), inputPositions, inputRadius);
+
+            NeuCor_Renderer brainRenderer(&brain);
+            brainRenderer.realRunspeed = false;
+            brainRenderer.runBrainOnUpdate = true;
+            brainRenderer.setDestructCallback(windowDestroy);
+
+            std::cout<<"Starting program loop\n";
+            while (!windowDestroyed) {
+                brainRenderer.pollWindow();
+                brainRenderer.updateView();
+            };
+            break;
         }
-        else {
-            inputs[0] = 0; inputs[1] = 0;
-        }*/
-        if (brain.getTime()>2000.0) inputs[0] = 200;
-        if (brain.getTime()>2000.0) inputs[2] = 100;
-        //if (rand()%100 == 0) inputs[2] = rand()%200;
-        //std::cout<<inputs[0]<<std::endl;
 
-        brainRenderer.pollWindow();
-        brainRenderer.updateView();
-    } while (!windowDestroyed);
+        case SIM_300_NEURONS:{
+            NeuCor brain(300);
+            NeuCor_Renderer brainRenderer(&brain);
+            brainRenderer.runBrainOnUpdate = true;
+            brainRenderer.realRunspeed = false;
+            brainRenderer.setDestructCallback(windowDestroy);
 
+            brain.runAll = true;
+            brain.runSpeed = 0.025;
+
+
+            float inputs[] = {(float) rand()/RAND_MAX*200.f, (float) rand()/RAND_MAX*200.f, (float) rand()/RAND_MAX*200.f};
+            brain.setInputRateArray(inputs, sizeof(inputs)/sizeof(float));
+
+            std::cout<<"Starting program loop\n";
+            while (!windowDestroyed) {
+                for (auto &i: inputs){ // Constantly change inputs
+                    i += ((float) rand()/RAND_MAX-0.5f)*2.f;
+                    i = std::min(std::max((double) i, 0.0), 220.0);
+                }
+
+                brainRenderer.pollWindow();
+                brainRenderer.updateView();
+            };
+
+            break;
+        }
+
+        default: {
+
+            NeuCor brain(500);
+            NeuCor_Renderer brainRenderer(&brain);
+            brainRenderer.runBrainOnUpdate = true;
+            brainRenderer.realRunspeed = false;
+            brainRenderer.setDestructCallback(windowDestroy);
+
+            brain.runAll = true;
+            brain.runSpeed = 0.05;
+
+
+            float inputs[] = {0, 0, 200};
+            coord3 inputPositions[] = {{1,0,0},{-2,0,0}, {-0.5, 1.5, 0}};
+            float inputRadius[] = {0.75, 1.0, 0.75};
+            brain.setInputRateArray(inputs, sizeof(inputs)/sizeof(float), inputPositions, inputRadius);
+
+            std::cout<<"Starting program loop\n";
+            unsigned t = 0;
+            while (!windowDestroyed) {
+                t++;
+                if (false) brain.run();
+
+                //if (brain.getTime() > 20){inputs[0] = 0; inputs[1] = 0;}
+                /*if (sin(brain.getTime()/30.0) > 0.0){
+                    inputs[0] = 100; inputs[1] = 100;
+                }
+                else {
+                    inputs[0] = 0; inputs[1] = 0;
+                }*/
+                if (brain.getTime()>2000.0) inputs[0] = 200;
+                if (brain.getTime()>2000.0) inputs[2] = 100;
+                //if (rand()%100 == 0) inputs[2] = rand()%200;
+                //std::cout<<inputs[0]<<std::endl;
+
+                brainRenderer.pollWindow();
+                brainRenderer.updateView();
+            };
+
+            break;
+        }
+    }
     return 0;
 }
